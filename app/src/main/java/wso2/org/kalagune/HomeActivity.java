adf18f7ced44b14925196e3bd1bd19e71af8d1fe
Your Name <kasun.mck@gmail.com>
2015-03-09 13:00:36 +0530

    Adding time updating function

--- app/src/main/java/wso2/org/kalagune/HomeActivity.java
+++ app/src/main/java/wso2/org/kalagune/HomeActivity.java
@@ -1,18 +1,52 @@
 package wso2.org.kalagune;
 
+import android.content.BroadcastReceiver;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import android.content.Context;
+import android.content.IntentFilter;
 
 
 public class HomeActivity extends ActionBarActivity {
+
+    private TextView textViewTime, textViewDate;
+    private BroadcastReceiver broadcastReceiver;
+    private static final String DATE_FORMAT = "EEEE, MMMM dd";
+    private static final String TIME_FORMAT = "HH:mm";
+    private SimpleDateFormat time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        textViewDate = (TextView)findViewById(R.id.textViewDate);
+        textViewTime = (TextView)findViewById(R.id.textViewTime);
+
+        time = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+        textViewDate.setText(time.format(new Date()));
+
+        time = new SimpleDateFormat(TIME_FORMAT, Locale.getDefault());
+        textViewTime.setText(time.format(new Date()));
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context ctx, Intent intent)
+            {
+                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0) {
+                    textViewTime.setText(time.format(new Date()));
+                }
+            }
+        };
+
+        registerReceiver(broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+
     }
 
 
@@ -38,5 +72,13 @@ public class HomeActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (broadcastReceiver != null) {
+            unregisterReceiver(broadcastReceiver);
+        }
+        super.onDestroy();
     }
 }
